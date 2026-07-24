e35320d7dec6bbe5421657b9369121500b04ce3d
buildfeatures <buildfeatures@gmail.com>
2026-07-24 12:16:36 +0530

    s

--- components/browse-more.js
+++ components/browse-more.js
@@ -4,7 +4,7 @@ async function loadBrowseMore() {
 
     if (!container) return;
 
-    const response = await fetch("https://buildfeatures.github.io/features/components/continue-exploring.html");
+    const response = await fetch("https://buildfeatures.github.io/features/components/browse-more.html");
 
     container.innerHTML = await response.text();
 
